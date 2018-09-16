0bb22d1d988032b291e2fc64f2590b48e7179770
a.plastinina <a.plastinina@gmail.com>
2018-09-16 13:40:29 +0300

    LibraryCash edit xml

--- LibraryCash.cs
+++ LibraryCash.cs
@@ -20,16 +20,40 @@ namespace ConsoleSearchAlbums
 
         public void Write(IEnumerable<IAlbum> albums)
         {
-            var xmlDocument = new XDocument(new XElement("albums-result"));
+            bool isNewDocument = !File.Exists(FilePathCash);
+            var xmlDocument = OpenDocument(isNewDocument);
             var root = xmlDocument.Root;
 
             foreach (var album in albums)
             {
-                root.Add(new XElement("album", 
-                    new XAttribute("artist", album.Artist),
-                    new XAttribute("name", album.Name)));
+                if (isNewDocument)
+                    AddAlbum(root, album);
+                else if (!ExistAlbum(root, album))
+                    AddAlbum(root, album);
+                else continue;
             }
             xmlDocument.Save(FilePathCash);
+        }
+
+        private bool ExistAlbum(XElement root, IAlbum album)
+        {
+            return root.Elements("albums").Any(
+                a => a.Attribute("artist").Value.ToLower() == album.Artist.ToLower()
+                    && a.Attribute("name").Value.ToLower() == album.Name.ToLower());
+        }
+
+        private static void AddAlbum(XElement root, IAlbum album)
+        {
+            root.Add(new XElement("album",
+                new XAttribute("artist", album.Artist),
+                new XAttribute("name", album.Name)));
+        }
+
+        private XDocument OpenDocument(bool isNewDocument)
+        {
+            return isNewDocument
+                    ? new XDocument(new XElement("albums-result"))
+                    : XDocument.Load(FilePathCash);
         }
 
         public IEnumerable<IAlbum> Read(string nameArtist)
